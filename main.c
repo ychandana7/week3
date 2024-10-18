4bf21ddfbff0f42fba62e3fb9a11c3b102e48007
ychandana7 <162562719+ychandana7@users.noreply.github.com>
2024-10-18 13:54:25 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "header.h"
+void main()
+{
+printf("\n i am in main.c\n");
+printf("\n calling hello function");
+hello;
+printf("\n calling add function\n");
+add(11,14);
+printf("\n back to main.c");
+}
